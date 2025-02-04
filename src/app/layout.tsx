abe38cf85dbaf434326113605804fae50007bd54
Scott Callaway <github@scottcallaway.co.uk>
2025-02-04 18:57:25 +0000

    Update metadata to something a bit more sensible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,15 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import React from "react";
 import localFont from "next/font/local";
 import "./global.scss";
+import { Metadata } from "next";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
 const officeCodePro = localFont({ src: "../fonts/OfficeCodeProD-Regular.otf" });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "scallaway.dev",
-  description: "Some online ramblings",
+  description: "An archive of my online ramblings",
 };
 
 export default function RootLayout({
