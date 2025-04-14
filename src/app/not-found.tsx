6f36426f51fc7f0d61c1850e8f10161fb19a6218
Scott Callaway <github@scottcallaway.co.uk>
2025-04-14 14:15:28 +0100

    Make the 404 page a lot nicer

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Custom404 = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      404 :: Page Not Found
+    </div>
+  );
+};
+
+export default Custom404;
